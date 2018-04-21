53e297f7dae5e083267f1325b2ea15a56f08320c
Chreem <549078191@qq.com>
2018-04-21 21:17:27 +0800

    chore: ts支持

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ let url = undefined;
 const Mix = require('./config/webpack.mix');
 
 // TODO 项目目录
-const sina = new Mix('./sina/20180411-hbh');
-sina.vue('index.js');
+const sina = new Mix('./project');
+sina.react('index.js');
 config = sina;
-url = 'http://n.sinaimg.cn/hb/hbh/';
+// url = 'http://n.sinaimg.cn/hb/hbh/';
 
 // TODO vendor在css加载上有bug待解决
 // .vendor(['react', 'jquery'], 'common')
